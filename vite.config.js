21d781c47f4c25c555f7999b45338728297249e9
g-szabolcs-b <s.gonczy@elsevier.com>
2026-03-25 16:26:51 +0100

    Executed the Hero section

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
+import injectHTML from 'vite-plugin-html-inject'
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     open: true
-  }
+  },
+  plugins: [
+    injectHTML(),
+  ],
 })
